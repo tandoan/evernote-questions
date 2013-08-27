2086270d9f8250b75e8575959f4322d6f1d45422
Tan Doan <tdoan@wearejustawesome.com>
2013-08-27 01:03:52 -0700

    dealing with possible overflow

--- MultiplyExceptSelf/3.php
+++ MultiplyExceptSelf/3.php
@@ -1,16 +1,35 @@
 <?php
 
 $numbers = array();
-$runningTotal = 1;
+$leftFactorials = array();
+$rightFactorials = array();
 
 $numLines = readline();
-for($i=0;$i < $numLines; $i++){
+
+$runningTotal = 1;
+$leftFactorials[0] = 1;
+$rightFactorials[$numLines] = 1;
+
+//store the left and right side multiplication in separate arrays to prevent overflow.
+//multiply the two arrays together for final answer
+
+$runCount = $numLines + 1;
+for($i=1;$i < $runCount; $i++){
 	$input = trim(readline());
-	$runningTotal *= $input;
 	$numbers[] = $input;
+	$leftFactorials[$i] = $leftFactorials[$i-1] * $input;
 }
 
-foreach($numbers as $num){
-	echo $runningTotal/$num;
-	echo "\n";
+// generate array for right side, using $numbers[] buffer
+for($i=1;$i < $runCount; $i++) {
+	$rightFactorials[$numLines - $i] = $rightFactorials[$numLines - $i + 1] * $numbers[$numLines - $i];
 }
+
+$i = 0;
+while($i < $numLines) {
+	echo $leftFactorials[$i] * $rightFactorials[$i+1];
+	echo "\n";
+	$i++;
+
+}
+
