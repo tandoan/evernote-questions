1d6f94a214b4fdd3dd21998640509ef35cc56a24
Tan Doan <tdoan@wearejustawesome.com>
2013-08-28 02:38:05 -0700

    removing newlines at end

--- MultiplyExceptSelf/3.php
+++ MultiplyExceptSelf/3.php
@@ -1,14 +1,14 @@
 <?php
 
 $numbers = array();
-$leftFactorials = array();
-$rightFactorials = array();
+$leftProducts = array();
+$rightProducts = array();
 
 $numLines = readline();
 
 $runningTotal = 1;
-$leftFactorials[0] = 1;
-$rightFactorials[$numLines] = 1;
+$leftProducts[0] = 1;
+$rightProducts[$numLines] = 1;
 
 //store the left and right side multiplication in separate arrays to prevent overflow.
 //multiply the two arrays together for final answer
@@ -17,19 +17,18 @@ $runCount = $numLines + 1;
 for($i=1;$i < $runCount; $i++){
 	$input = trim(readline());
 	$numbers[] = $input;
-	$leftFactorials[$i] = $leftFactorials[$i-1] * $input;
+	$leftProducts[$i] = $leftProducts[$i-1] * $input;
 }
 
 // generate array for right side, using $numbers[] buffer
 for($i=1;$i < $runCount; $i++) {
-	$rightFactorials[$numLines - $i] = $rightFactorials[$numLines - $i + 1] * $numbers[$numLines - $i];
+	$rightProducts[$numLines - $i] = $rightProducts[$numLines - $i + 1] * $numbers[$numLines - $i];
 }
 
 $i = 0;
+$output = array();
 while($i < $numLines) {
-	echo $leftFactorials[$i] * $rightFactorials[$i+1];
-	echo "\n";
+	$output[$i] = $leftProducts[$i] * $rightProducts[$i+1];
 	$i++;
-
 }
-
+echo join("\n", $output);
