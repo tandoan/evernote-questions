d70c35056e2be70daa702f1005ef1118687a7c50
Tan Doan <tdoan@wearejustawesome.com>
2013-08-30 02:29:12 -0700

    trying different algorithm for multiplyexceptself

--- MultiplyExceptSelf/3.php
+++ MultiplyExceptSelf/3.php
@@ -2,33 +2,30 @@
 
 $numbers = array();
 $leftProducts = array();
-$rightProducts = array();
 
 $numLines = readline();
 
 $runningTotal = 1;
 $leftProducts[0] = 1;
-$rightProducts[$numLines] = 1;
 
-//store the left and right side multiplication in separate arrays to prevent overflow.
-//multiply the two arrays together for final answer
-
+//store products from left to right
 $runCount = $numLines + 1;
 for($i=1;$i < $runCount; $i++){
 	$input = trim(readline());
-	$numbers[] = $input;
 	$leftProducts[$i] = $leftProducts[$i-1] * $input;
+	$numbers[] = $input;
 }
 
-// generate array for right side, using $numbers[] buffer
-for($i=1;$i < $runCount; $i++) {
-	$rightProducts[$numLines - $i] = $rightProducts[$numLines - $i + 1] * $numbers[$numLines - $i];
+//step backwards, calculating product from right to left, store in place
+$rightProducts = 1;
+for($i = $numLines-2;$i>=0;$i--) {
+	$rightProducts = $rightProducts * $numbers[$i+1];
+	$leftProducts[$i] = $leftProducts[$i] * $rightProducts;
+
 }
 
-$i = 0;
-$output = array();
-while($i < $numLines) {
-	$output[$i] = $leftProducts[$i] * $rightProducts[$i+1];
-	$i++;
-}
-echo join("\n", $output);
+//last element unnecessary
+array_pop($leftProducts);
+
+//final output
+echo join(PHP_EOL, $leftProducts);
