4d60dcad0dedb86eedd92c2814e6cff0b5cabb5f
Tan Doan <tdoan@wearejustawesome.com>
2013-08-31 01:24:36 -0700

    using GMP lib to deal with overflow on 32bit machines

--- MultiplyExceptSelf/3.php
+++ MultiplyExceptSelf/3.php
@@ -12,15 +12,15 @@ $leftProducts[0] = 1;
 $runCount = $numLines + 1;
 for($i=1;$i < $runCount; $i++){
 	$input = trim(readline());
-	$leftProducts[$i] = $leftProducts[$i-1] * $input;
+	$leftProducts[$i] = gmp_mul($leftProducts[$i-1], $input);
 	$numbers[] = $input;
 }
 
 //step backwards, calculating product from right to left, store in place
 $rightProducts = 1;
 for($i = $numLines-2;$i>=0;$i--) {
-	$rightProducts = $rightProducts * $numbers[$i+1];
-	$leftProducts[$i] = $leftProducts[$i] * $rightProducts;
+	$rightProducts = gmp_mul($rightProducts,  $numbers[$i+1]);
+	$leftProducts[$i] = gmp_mul($leftProducts[$i], $rightProducts);
 
 }
 
@@ -28,4 +28,11 @@ for($i = $numLines-2;$i>=0;$i--) {
 array_pop($leftProducts);
 
 //final output
-echo join(PHP_EOL, $leftProducts);
+$i = 0;
+while($i<$numLines){
+	echo gmp_strval($leftProducts[$i]);
+	$i++;
+	if($i<$numLines){
+		echo PHP_EOL;
+	}
+}
